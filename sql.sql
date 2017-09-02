cd71fed4f71a3ee1eb5978b326d74717b434f3db
Tree&Saw <kmj070444@naver.com>
2017-09-02 03:52:28 -0700

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS `businesses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `latitude` float(10,6) NOT NULL,
+  `longitude` float(10,6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (25, 'CSUSB', 34.177490, -117.3212208); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (1, 'CSUSB', 34.132490, -117.3122908); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (2, 'CSUSB', 34.1232490, -117.122234108); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (3, 'CSUSB', 34.140, -117.1220908); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (4, 'CSUSB', 34.17234290, -117.12220908); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (5, 'CSUSB', 34.14290, -117.31221208); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (6, 'CSUSB', 34.162490, -117.320212418); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (7, 'CSUSB', 34.174490, -117.312023418); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (8, 'CSUSB', 34.11630, -117.320921241); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (9, 'CSUSB', 34.135390, -117.321234108); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (10, 'CSUSB', 34.13450, -117.322341128); 
+INSERT INTO `businesses` (`id`, `name`, `latitude`, `longitude`) VALUES (11, 'Hodory', 34.033869, -118.246879);
